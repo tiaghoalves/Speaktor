7d618af63ae224f8f051064c102d07793ca39e42
Daniele Paz <daniele.m.paz@outlook.com>
2015-12-02 21:08:30 -0200

    Primeiro commit com a base

---
+++ speaktor/src/main/java/br/com/cwi/crescer/speaktor/App.java
@@ -0,0 +1,13 @@
+package br.com.cwi.crescer.speaktor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
